bd6d0e10e95eff9ab480c1c21c513cb93b1e5ef4
HHHHHeloise <guirong.wu.1@stonybrook.edu>
2024-05-31 21:43:11 -0400

    review-list-done

--- src/components/Restaurants/ReviewPage.js
+++ src/components/Restaurants/ReviewPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './OverallRating.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faCamera } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faCamera, faSearch, faCaretDown, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import StarRatingInput from './StarRatingInput';
 
 const UserReviewPrompt = () => {
@@ -60,34 +60,68 @@ const OverallRating = () => (
         </div>
     </div>
 );
+
 const ReviewFilters = () => (
     <div className="review-filters">
-        <button className="filter-button">Yelp Sort</button>
-        <button className="filter-button">Filter by rating</button>
+        <div className="filter-buttons">
+            <button className="filter-button">
+                Yelp Sort
+                <FontAwesomeIcon icon={faCaretDown} className="caret-icon" />
+            </button>
+            <button className="filter-button">Filter by rating
+                <FontAwesomeIcon icon={faCaretDown} className="caret-icon" />
+            </button>
+        </div>
+        <div className="search-reviews">
+            <input type="text" placeholder="Search reviews" />
+            <FontAwesomeIcon icon={faSearch} />
+        </div>
     </div>
 );
 
 const ReviewList = () => (
     <div className="review-list">
         <div className="review-item">
-            <div className="review-avatar"></div>
+            <div className="review-avatar">
+                <img
+                    src="https://s3-media0.fl.yelpcdn.com/assets/public/default_user_avatar_64x64_v2.yji-19e0a8ff85b15f4bbd79.png"
+                    alt="Review Avatar"
+                    className="review-avatar-img"
+                />
+            </div>
             <div className="review-content">
-                <h5>Colette G.</h5>
-                <p>Shoreham, NY</p>
+                <div className="review-header">
+                    <h5>Colette G.</h5>
+                    <span className="user-tag">Elite 24</span>
+                </div>
+                <p className="user-location">Shoreham, NY</p>
                 <div className="review-metrics">
-                    <span>162</span>
-                    <span>500</span>
-                    <span>461</span>
+                    <span>162 <FontAwesomeIcon icon={faCamera} /></span>
+                    <span>500 <FontAwesomeIcon icon={faStar} /></span>
+                    <span>462 <FontAwesomeIcon icon={faStar} /></span>
                 </div>
                 <div className="review-rating">
-                    {[1, 2, 3, 4, 5].map((star) => (
-                        <FontAwesomeIcon icon={faStar} key={star} className="star-icon" />
+                    {[1, 2, 3, 4, 5].map((star, index) => (
+                        <FontAwesomeIcon icon={faStar} key={index} className="star-icon" />
                     ))}
                 </div>
                 <p className="review-date">Dec 10, 2021</p>
                 <p className="review-text">
                     This review is strictly for chicken sandwich, and the drive thru. WOW! Those commercials you see of people moaning in their vehicle over how good it is...is no lie!
                 </p>
+                <p className="review-text">
+                    My family hates Popeyes. I love it. Happened to be in area, and alone..so drive thru sandwich as it's more car friendly than a chicken meal.
+                </p>
+                <p className="review-text">
+                    I'm blown away. So so good.
+                </p>
+                <div className="review-actions">
+                    <FontAwesomeIcon icon={faEllipsisH} className="ellipsis-icon" />
+                    <div className="action-menu">
+                        <div className="action-item">Share review</div>
+                        <div className="action-item">Embed review</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
