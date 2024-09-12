53bb018ab527c1b0dff9f2f7a1e9932db22f0e5f
HHHHHeloise <guirong.wu.1@stonybrook.edu>
2024-09-11 22:27:43 -0400

    deploy

--- src/components/Restaurants/RestaurantDetail.js
+++ src/components/Restaurants/RestaurantDetail.js
@@ -1,5 +1,5 @@
 import React , { useState, useEffect } from 'react';
-import moment from 'moment/moment';
+import moment from 'moment';
 import { FaBook, FaCamera } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
--- src/components/Restaurants/WriteReviewPage.js
+++ src/components/Restaurants/WriteReviewPage.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaYelp } from 'react-icons/fa';
+import { IoMdAddCircle } from 'react-icons/io';
 import StarRatingInput from './StarRatingInput'; 
 import './WriteReviewPage.css';
 import './Restaurants.css';
+import ImageUploader from './ImageUploader';
 import { useAuth } from '../Auth/AuthContext';
 
 function WriteReviewPage() {
@@ -14,8 +16,30 @@ function WriteReviewPage() {
     const { restaurantId } = useParams();
     const [body, setBody] = useState("");
     const [score, setScore] = useState(0);
+    const [images, setImages] = useState([]);
     const [restaurantName, setRestaurantName] = useState("");
     const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+
+    const modalRef = useRef();
+
+    const handleOutsideClick = (event) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            setShowModal(false);
+        }
+    };
+
+    useEffect(() => {
+        if (showModal) {
+            document.addEventListener('mousedown', handleOutsideClick);
+        } else {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [showModal]);
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/v1/restaurants/${restaurantId}`)
@@ -30,7 +54,23 @@ function WriteReviewPage() {
         setBody(event.target.value);
     };
 
-    const handleSubmit = (event) => {
+    const uploadImages = async () => {
+        const formData = new FormData();
+        images.forEach(image => formData.append('images', image));
+
+        const response = await fetch(`http://localhost:8080/api/v1/photos/upload/${restaurantId}`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+            body: formData
+        });
+
+        if (!response.ok) throw new Error('Failed to upload images');
+        return response.json(); // Assuming this returns an array of image URLs
+    };
+
+    const handleSubmit = async(event) => {
         event.preventDefault();
         if (body.trim() === "" || score === 0) {
             alert("Please enter a review and select a score before submitting.");
@@ -42,39 +82,66 @@ function WriteReviewPage() {
             return;
         }
 
-        console.log('Attempting to submit review with token:', token);
-
-        const parsedRestaurantId = Number(restaurantId);
-
-        const reviewData = {
-            restaurantId: parsedRestaurantId, 
-            userId: authContext.userId, 
-            body,
-            score,
-        };
-
-        fetch('http://localhost:8080/api/v1/reviews/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(reviewData),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to submit review');
+        try {
+            let imageUrls = [];
+            if (images.length > 0) {
+                imageUrls = await uploadImages(); // Upload images first
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Review submitted successfully:', data);
-            navigate('/detail/write-review/success'); 
-        })
-        .catch(error => {
-            console.error('Error submitting review:', error);
-            alert('Error submitting review. Please try again.');
-        });
+
+            const reviewData = new FormData();
+            reviewData.append('body', body);
+            reviewData.append('score', score);
+            reviewData.append('restaurantId', restaurantId);
+            imageUrls.forEach(url => reviewData.append('imageUrls', url)); // Append image URLs to the review data
+
+            const reviewResponse = await fetch('http://localhost:8080/api/v1/reviews/add', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${authContext.token}`
+                },
+                body: reviewData
+            });
+
+            if (!reviewResponse.ok) throw new Error('Failed to submit review');
+
+            alert('Review and images uploaded successfully!');
+            navigate(`/detail/${restaurantId}`);
+        } catch (error) {
+            console.error('Error submitting review and images:', error);
+            alert('Error submitting review and images. Please try again.');
+        }
+
+        // console.log('Attempting to submit review with token:', token);
+
+        // event.preventDefault();
+        // const reviewData = new FormData(); // Use FormData to handle text and file data
+        // reviewData.append('body', body);
+        // reviewData.append('score', score);
+        // reviewData.append('userId', authContext.userId);
+        // reviewData.append('restaurantId', restaurantId);
+
+        // fetch('http://localhost:8080/api/v1/reviews/add', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': `Bearer ${token}`
+        //     },
+        //     body: JSON.stringify(reviewData),
+        // })
+        // .then(response => {
+        //     if (!response.ok) {
+        //         throw new Error('Failed to submit review');
+        //     }
+        //     return response.json();
+        // })
+        // .then(data => {
+        //     console.log('Review submitted successfully:', data);
+        //     navigate('/detail/write-review/success'); 
+        // })
+        // .catch(error => {
+        //     console.error('Error submitting review:', error);
+        //     alert('Error submitting review. Please try again.');
+        // });
     };
 
 
@@ -86,12 +153,31 @@ function WriteReviewPage() {
             <div className="write-review-page">
                 <h2>{restaurantName}</h2>
                 <form onSubmit={handleSubmit} className="review-form">
+                    {/* <!-- Review text section --> */}
                     <div className="review-box">
                         <StarRatingInput setRating={setScore} />
                         <textarea value={body} onChange={handleInputChange} placeholder="Share your experience..." className="review-textarea"></textarea>
                     </div>
                     <button type="submit" className="post-review-button">Post Review</button>
                 </form>
+                    {/* <!-- Attach photos section --> */}
+                    <label className="attach-photos-label">Attach photos</label>
+                    <div className="photos-box">
+                        <div className="file-upload-container">
+                            <label htmlFor="file-upload" className="custom-file-upload">
+                                <IoMdAddCircle size={24} color="black" onClick={() => setShowModal(true)} />
+                            </label>
+                        </div>
+                    </div>
+                    {/* <button type="submit" className="post-review-button">Post Review</button> */}
+                
+                {showModal && (
+                    <div className="modal">
+                        <div ref={modalRef} className="modal-content">
+                            <ImageUploader />
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
